453013907b607bcc81e76953241211d789c10634
Hongyuan Mei <hongyuanmei@gmail.com>
2022-04-30 22:41:39 -0500

    not use multigpu

--- pytorch/run_wt103_base.sh
+++ pytorch/run_wt103_base.sh
@@ -22,7 +22,6 @@ if [[ $1 == 'train' ]]; then
         --mem_len 150 \
         --eval_tgt_len 150 \
         --batch_size 60 \
-        #--multi_gpu \
         --gpu0_bsz 4 \
         ${@:2}
 elif [[ $1 == 'eval' ]]; then
